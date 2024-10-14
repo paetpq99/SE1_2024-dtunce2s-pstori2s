eb587c08541bdcb304f8cd40d5447e1c4df71c4b
paetpq99 <161954013+paetpq99@users.noreply.github.com>
2024-10-14 21:18:18 +0200

    Delete codesSEws24 directory

--- codesSEws24/src/org/hbrs/se1/ws24/exercises/uebung1/ContainerException.java
+++
@@ -1,7 +0,0 @@
-package org.hbrs.se1.ws24.exercises.uebung1.uebung2;
-
-public class ContainerException extends Exception {
-    public ContainerException(String message) {
-        super(message);
-    }
-}
--- codesSEws24/src/org/hbrs/se1/ws24/exercises/uebung1/control/Factory.java
+++
@@ -1,7 +0,0 @@
-package org.hbrs.se1.ws24.exercises.uebung1.control;
-
-public class Factory {
-    public static Translator createGermanTranslator() {
-        return new GermanTranslator();
-    }
-}
